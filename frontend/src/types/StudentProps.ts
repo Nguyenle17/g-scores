20e67d72f8dfc3076dbd09d85d929d1452aebf54
Nguyen <nguyenlenguyen1712004@gmail.com>
2026-07-16 16:30:58 +0700

    missing some file

---
+++ frontend/src/types/StudentProps.ts
@@ -0,0 +1,15 @@
+export interface StudentProps {
+  sbd: string;
+  toan?: number;
+  nguVan?: number;
+  ngoaiNgu?: number;
+  vatLi?: number;
+  hoaHoc?: number;
+  sinhHoc?: number;
+  lichSu?: number;
+  diaLi?: number;
+  gdcd?: number;
+  maNgoaiNgu?: string;
+  topScoreGroupA?: boolean;
+  rank?: number;
+}
